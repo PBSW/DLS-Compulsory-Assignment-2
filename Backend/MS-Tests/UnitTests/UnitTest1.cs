be4cffee2340fa2ab2f0ece1b16d6c72054f4c3a
Philip Zadeh <PhilEZad@gmail.com>
2024-04-17 17:30:44 +0200

    Added test projects

---
+++ Backend/MS-Tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace MS_Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Backend/PS-Tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace PS_Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
