6e37fa953ad3bfd66268496dc9249adeffd64f05
Philip Zadeh <90150224+PhilEZad@users.noreply.github.com>
2024-04-22 13:06:38 +0200

    Configured properties on Patients for EF

--- Backend/PS-Infrastructure/DatabaseContext.cs
+++ Backend/PS-Infrastructure/DatabaseContext.cs
@@ -1,10 +1,34 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Shared;
 
 namespace PS_Infrastructure;
 
 public class DatabaseContext : DbContext
 {
-    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        //Primary Keys
+        modelBuilder.Entity<Patient>()
+            .HasKey(p => p.Ssn);
+        
+        //Properties
+        modelBuilder.Entity<Patient>()
+            .Property(p => p.Ssn)
+            .IsRequired()
+            .HasColumnType("varchar(10)");
+        
+        modelBuilder.Entity<Patient>()
+            .Property(p => p.Mail)
+            .IsRequired()
+            .HasColumnType("varchar(128)");
+        
+        modelBuilder.Entity<Patient>()
+            .Property(p => p.Name)
+            .IsRequired()
+            .HasColumnType("varchar(128)");
     }
+    
+    public DbSet<Patient> Patients { get; set; }
 }
