ad439f40669a8d1efce0a2c27dd3eb8656dbbec0
Rasmus Sandbæk <sandbxk@gmail.com>
2024-04-22 10:18:33 +0200

    fix status code for http

--- Frontend/PatientUI/src/services/measurement.service.ts
+++ Frontend/PatientUI/src/services/measurement.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Measurement } from '../domain/measurement';
 import { catchError, map, Observable } from 'rxjs';
@@ -11,21 +11,13 @@ export class MeasurementService {
   constructor(private http: HttpClient) { }
 
   postMeasurement(measurement: Measurement): Observable<boolean> {
-    return this.http.post('/measurements', measurement)
+    return this.http.post<HttpResponse<any>>('/measurements', measurement)
     .pipe(
-      catchError((error) => {
-        console.error(error);
-        return [error];
-      }),
       map((response) => {
-      if (response instanceof Error
-        || response instanceof ErrorEvent
-        || response === undefined
-        || response === null
-        || response === false) {
-        return false;
+      if (response.ok) {
+        return true;
       }
-      return true;
+      else return false;
     }))
   }
 
