ce65221076d3d452d6263b46cfc263d381d2f64e
Philip Zadeh <90150224+PhilEZad@users.noreply.github.com>
2024-04-26 17:28:01 +0200

    [FIX] Added missing Measurement validation checks

--- Backend/MS-Application/Helpers/MeasurementValidators.cs
+++ Backend/MS-Application/Helpers/MeasurementValidators.cs
@@ -14,6 +14,13 @@ public class MeasurementValidators : AbstractValidator<Measurement>
             .NotEmpty().WithMessage("SSN is required")
             .MaximumLength(10).WithMessage("SSN may not be longer than 10 numbers")
             .Matches(@"^\d+$").WithMessage("SSN may only contain numbers");
+        
+        RuleFor(m => m.Systolic)
+            .NotNull().WithMessage("Systolic is null")
+            .GreaterThan(0).WithMessage("Systolic must be greater than 0");
+        
+        RuleFor(m => m.Diastolic)
+            .NotNull().WithMessage("Diastolic is null")
+            .GreaterThan(0).WithMessage("Diastolic must be greater than 0");
     }
-    
 }
